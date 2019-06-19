0d59c596f91891fac0ead365a3274ea52fc32114
jsownz <jason@frozensoliddesigns.com>
2019-06-18 22:12:04 -0400

    formatting output a little better

--- network_scanner.py
+++ network_scanner.py
@@ -6,9 +6,10 @@ def scan(ip_address):
     arp_request = scapy.ARP(pdst=ip_address)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast,timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
 
+    print("IP\t\t\tMAC Address\n")
     for answer in answered_list:
-        print("[+] " + answer[1].psrc + " : " + answer[1].hwsrc)
+        print(answer[1].psrc + "\t\t" + answer[1].hwsrc)
 
 scan("10.0.2.1/24")
